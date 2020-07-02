a7592e0583b0d2b4e18787610097a2c4dc6031ba
Emmet Allen <Emmet.Allen@gmail.com>
2020-07-02 17:50:14 -0400

    Update of Both List_Of_Techniques.txt and NageWaza.js
    In List_Of_Techniques.txt:
    
    - Added Bangs (!) to moves that are
    added to JudoDictionary
    
    In NageWaza.js:
    
    - Changed order of constructor arguments
    
    - Added 4 more techniques

--- Techniques/NageWaza.js
+++ Techniques/NageWaza.js
@@ -1,22 +1,37 @@
-function JudoDictionary{
+function JudoDictionary(){
 
 
-				function NageWaza(techniqueType,name,beltGroup,translation,regulation,recognized,officalLink){
-			//Type of Technique
-				this.techniqueType = techniqueType
+				function NageWaza(name,translation,techniqueType,beltGroup,regulation,recognized,references){
 				this.name = name;
-			//Group number if technique is recognized
-				this.beltGroup = beltGroup;
 			// Rough English translation
 				this.translation = translation;
+			//Type of Technique
+				this.techniqueType = techniqueType	
+			//Group number if technique is recognized
+				this.beltGroup = beltGroup;
+			//Is the tournament regulation of technique
 				this.regulation = regulation;
-			//True or False	
-				this.recognized= true || false;
+			//Is it offically recognized by the Kodokan
+				this.recognized= recognized;
+			//Reference Materials
+				this.references = references;
 					};
-
-				let ipponSeoiNage = new Tewaza("Te-Waza","Ippon Seoi Nage", "Dai Ikkyo", "Shoulder Throw", "Accepted", true);
-
-
-				let kataGuruma = new Tewaza("Te-Waza","Kata Guruma", "Dai Sankyo", "Shoulder Wheel", "Accepted", true);
+			
+			//Te-Waza (Hand - Techniques)
+				let ipponSeoiNage = new NageWaza("Ippon Seoi Nage", "One-Arm Shoulder Throw", "Te-Waza", "Dai Ikkyo", "Accepted", "Offically Recognized", "http://kodokanjudoinstitute.org/en/waza/digest/02/");
+				
+				let kataGuruma = new NageWaza("Kata Guruma","Shoulder Wheel", "Te-Waza", "Dai Sankyo",  "Accepted", "Offically Recognized", "http://kodokanjudoinstitute.org/en/waza/digest/10/");
+				
+				let kibisuGaeshi = new NageWaza("Kibisu Gaeshi", "Ankle Pick", "Te-Waza", "Shinmeisho No Waza", "Prohibited", "Offically Recognized", "http://kodokanjudoinstitute.org/en/waza/digest/13/");
+				
+				let moroteGari = new NageWaza("Morote Gari", "Two-Hands Reap", "Te-Waza", "Shinmeisho No Waza", "Prohibited", "Offically Recognized", "http://kodokanjudoinstitute.org/en/waza/digest/07/");
+				
+				let obiOtoshi = new NageWaza("Obi Otoshi", "Belt Drop", "Te-Waza", "Habukareta-Waza", "Prohibited", "Offically Recognized", "http://kodokanjudoinstitute.org/en/waza/digest/06/");
+				
+				let seoiOtoshi = new NageWaza("Seoi Otoshi", "Shoulder Drop", "Te-Waza", "Habukareta-Waza", "Accepted", "Offically Recognized", "http://kodokanjudoinstitute.org/en/waza/digest/03/");
 }
 
+
+				
+
+	
